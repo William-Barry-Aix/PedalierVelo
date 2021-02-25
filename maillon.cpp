08fc2de2d497d803ebd316a1c61bfad5e9a3b724
yrhden <guenolecoeffic@gmail.com>
2021-02-25 17:14:46 +0100

    Gueno
    Commit sans pré test du maillon

--- maillon.cpp
+++ maillon.cpp
@@ -41,20 +41,17 @@ void Maillon::initPoints(){
     this->vertices = new GLfloat[nbPtsFace*3 + nbPtsFacette*3];
     this->colors = new GLfloat[nbPtsFace*3 + nbPtsFacette*3];
 
-    QVector3D A = QVector3D((x_width/2) - bevel_size, y_height/2, z_ep/2);
+    QVector3D A = QVector3D((x_width/2) - bevel_size, -y_height/2, z_ep/2);
+    QVector3D B = QVector3D(x_width/2, -(y_height/2) + bevel_size, z_ep/2);
 
-    QVector3D A = getVertex(0, 0, ep_cyl/2);//QVector3D(0,0,ep_cyl/2);
-    QVector3D B = getVertex(0,r_cyl - h_dent/2, ep_cyl/2);//QVector3D(0,r_cyl - h_dent/2,-ep_cyl/2);
+    QVector3D C = QVector3D(x_width/2, (y_height/2) - bevel_size, z_ep/2);
+    QVector3D D = QVector3D((x_width/2) - bevel_size, y_height/2, z_ep/2);
 
+    QVector3D E = QVector3D((-x_width/2) + bevel_size, y_height/2, z_ep/2);
+    QVector3D F = QVector3D(-x_width/2, (y_height/2) - bevel_size, z_ep/2);
 
-    QVector3D C = getVertex(alpha/4,r_cyl - h_dent/2, ep_cyl/2);//QVector3D(alpha/4,r_cyl - h_dent/2,ep_cyl/2);
-    QVector3D D = getVertex((2*alpha)/4,r_cyl + h_dent/2, ep_cyl/2);//QVector3D((2*alpha)/4,r_roue + h_dent/2,-ep_cyl/2);
-
-    QVector3D E = getVertex((3*alpha)/4, r_cyl + h_dent/2, ep_cyl/2);//QVector3D((2*alpha)/4,r_roue + h_dent/2,-ep_cyl/2);
-    QVector3D F = getVertex(alpha, r_cyl - h_dent/2, ep_cyl/2);//QVector3D((2*alpha)/4,r_roue + h_dent/2,-ep_cyl/2);
-
-    QVector3D G = getVertex(alpha, 0, ep_cyl/2);//QVector3D((2*alpha)/4,r_roue + h_dent/2,-ep_cyl/2);
-    QVector3D H = getVertex((2*alpha)/4, 0, ep_cyl/2);//QVector3D((2*alpha)/4,r_roue + h_dent/2,-ep_cyl/2);
+    QVector3D G = QVector3D(-x_width/2, (-y_height/2) + bevel_size, z_ep/2);
+    QVector3D H = QVector3D((-x_width/2) + bevel_size, -y_height/2, z_ep/2);
 
 
     QVector3D Ap = A;
@@ -66,14 +63,14 @@ void Maillon::initPoints(){
     QVector3D Gp = G;
     QVector3D Hp = H;
 
-    Ap.setZ(-ep_cyl/2);
-    Bp.setZ(-ep_cyl/2);
-    Cp.setZ(-ep_cyl/2);
-    Dp.setZ(-ep_cyl/2);
-    Ep.setZ(-ep_cyl/2);
-    Fp.setZ(-ep_cyl/2);
-    Gp.setZ(-ep_cyl/2);
-    Hp.setZ(-ep_cyl/2);
+    Ap.setZ(z_ep/2);
+    Bp.setZ(z_ep/2);
+    Cp.setZ(z_ep/2);
+    Dp.setZ(z_ep/2);
+    Ep.setZ(z_ep/2);
+    Fp.setZ(z_ep/2);
+    Gp.setZ(z_ep/2);
+    Hp.setZ(z_ep/2);
 
     QVector3D color = QVector3D(coul_r, coul_v, coul_b);
 
@@ -128,4 +125,41 @@ void Maillon::buildVertData(QVector<GLfloat> &data)
         data.append(colors[i]);
     }
     qDebug() << data.length() << " " << verticesCpt+colorsCpt;
+}
+
+
+void Maillon::draw(QOpenGLShaderProgram *m_program, QMatrix4x4 matrix, int m_matrixUniform){
+    matrix.rotate(0, 0, 0, 1);
+    m_program->setUniformValue(m_matrixUniform, matrix);
+    drawBlock();
+}
+
+
+void Maillon::drawBlock(){
+    glDrawArrays(GL_POLYGON, 0, nbPtsFace/2);
+    glDrawArrays(GL_POLYGON, nbPtsFace/2, nbPtsFace/2);
+    glDrawArrays(GL_QUADS, nbPtsFacette, nbPtsFacette);
+}
+
+void Maillon::drawBlock(QOpenGLShaderProgram *program, QOpenGLFunctions *glFuncs)
+{
+    m_vbo.bind();
+
+    program->setAttributeBuffer("posAttr",
+        GL_FLOAT, 0 * sizeof(GLfloat), 3, 3 * sizeof(GLfloat));
+    program->setAttributeBuffer("colAttr",
+        GL_FLOAT, verticesCpt * sizeof(GLfloat), 3, 3 * sizeof(GLfloat));
+    program->enableAttributeArray("posAttr");
+    program->enableAttributeArray("colAttr");
+
+    // Pour des questions de portabilité, hors de la classe GLArea, tous les appels
+    // aux fonctions glBidule doivent être préfixés par glFuncs->.
+    glFuncs->glDrawArrays(GL_POLYGON, 0, nbPtsFace/2);
+    glFuncs->glDrawArrays(GL_POLYGON, nbPtsFace/2, nbPtsFace/2);
+    glFuncs->glDrawArrays(GL_QUADS, nbPtsFacette, nbPtsFacette);
+
+    program->disableAttributeArray("posAttr");
+    program->disableAttributeArray("colAttr");
+
+    m_vbo.release();
 }
